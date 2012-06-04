acbc56128760f7ffb5d1eee38f37293b89d2c6c8
Mark Hepburn <Mark.Hepburn@gmail.com>
2012-06-04 20:34:23 +1000

    Moving slimbox-loading code into its own file (and adding a title to images if present)

---
+++ assets/themes/mark-reid/js/slimbox-integration.js
@@ -0,0 +1,24 @@
+$(function() {
+  var picasaUserId = '110262280296887306226';
+  $('.gallery').each(function(_i, el) {
+    var $el = $(el),
+    albumId = $el.attr('data-album'); // Not .data('album') unfortunately, or coercion can truncate it
+    $el.picasaGallery(picasaUserId, albumId, function(images) {
+      $ul = $('<ul class="picasa-album"></ul>');
+      $.each(images, function(i, img) {
+        var $li  = $('<li class="picasa-image"></li>'),
+            $a   = $('<a class="picasa-image-large"></a>').attr('href', img.url),
+            $img = $('<img class="picasa-image-thumb" />').attr('src', img.thumbs[1].url);
+        if (!/\.(jpg|png|gif)$/i.test(img.title)) {
+          $a.attr('title', img.title);
+        }
+        $a.append($img); $li.append($a); $ul.append($li);
+      });
+      $el.append($ul).find('a').slimbox({
+        loop: true,
+        resizeDuration: 1,
+        imageFadeDuration: 1
+      });
+    });
+  });
+});
